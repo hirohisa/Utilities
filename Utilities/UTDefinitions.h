44a2319d248d046d826e49f92bd38a32cde40a4f
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2013-05-18 11:29:47 +0900

    add defines use on all project

--- Utilities/UTDefinitions.h
+++ Utilities/UTDefinitions.h
@@ -7,3 +7,29 @@
 //
 
 extern void SwizzleInstanceMethod(Class c, SEL original, SEL alternative);
+
+// logging
+#ifdef DEBUG
+# define DEBUG_LOG NSLog(@"%s", __func__)
+# define LOG(fmt, ...) NSLog((@"%s" fmt), __PRETTY_FUNCTION__, __VA_ARGS__)
+#else
+# define DEBUG_LOG ;
+# define LOG(...) ;
+#endif
+
+//
+#define SCREEN_SIZE [[UIScreen mainScreen] bounds].size
+#define __IS_RETINA__ [UIScreen mainScreen].scale == 2.
+
+//
+#define __OS_VERSION__ [[[UIDevice currentDevice] systemVersion] floatValue]
+#define OS_REQUIRED(VERSION) __OS_VERSION__ >= VERSION
+
+// weak, block
+#if __has_feature(objc_arc_weak) // iOS 5 or above
+#define __block_weak        __weak
+#elif __has_feature(objc_arc)    // iOS 4.X
+#define __block_weak        __block
+#else                            // iOS 3.X or non-ARC projects
+#define __block_weak        __block
+#endif
